bc2caa701d8f856c6a4ec1329fbbec325d1e8f95
faneva <rherintsoafaneva@gmail.com>
2025-01-12 19:48:09 +0300

    merge the main's harilala into main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: frahenin <frahenin@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 11:21:53 by nrasamim          #+#    #+#             */
-/*   Updated: 2025/01/12 19:42:51 by frahenin         ###   ########.fr       */
+/*   Updated: 2025/01/12 19:45:38 by frahenin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,11 @@ int	main(int ac, char **av, char **envp)
 		{
 			add_history(input);
 			shell.cmd = parsing(&shell, input);
-			// if (shell.cmd)
-			// 	execute_command(shell.cmd, shell);
+			if (!execute_command(&shell))
+				{
+					perror("Error during excecution");
+					break ;
+				}
 			ft_free_cmd(&shell.cmd);
 		}
 		ft_free(input);
